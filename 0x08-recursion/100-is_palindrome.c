130c962ae24433374cdc47c4dbad487ceeac4acf
your_username <your_email_address@example.com>
2021-09-15 21:44:12 +0200

    My first commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,17 +1,47 @@
 #include "main.h"
+
 /**
- * is_palindrome - find a palindrome string
- * @s: string for check
- *
- * Return: 1 if is palindrome, otherwise 0
+ * compare - compare head and tail indices for match
+ * @head: index starting from left of string
+ * @tail: index starting from right of string, moving backwards
+ * Return: 1 if palindrome, 0 if not
  */
+
+int compare(char *head, char *tail)
+{
+
+	if (head >= tail)
+		return (1);
+	if (*head == *tail)
+		return (compare(head + 1, tail - 1));
+
+	return (0);
+}
+
+/**
+ * _strlen - find length of string to access last index
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (1 + (_strlen(s)));
+}
+
+/**
+ * is_palindrome - check if palindrome
+ * @s: string to check
+ * Return: 1 if palindrome, 0 if not
+ */
+
 int is_palindrome(char *s)
 {
-	int i;
+	int len = _strlen(s);
 
-	i = 0;
-
-	if (*s == i)
-		return (0);
-	return (1);
+	return (compare(s, (s + len - 1)));
 }
