245f7099a42fe64938bbc69f99848bcef13914f7
Chavu-1 <kaluamadalitso@yahoo.com>
2021-10-12 09:54:30 +0200

    first commit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,79 +1,48 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes node at index
- * @head: head
- * @index: index
- * Return: 1 if succeeded, -1 otherwise
+ * delete_nodeint_at_index - delete a node at a given position
+ * @head: pointer to head pointer of linked list
+ * @index: index to delete node
+ * Return: 1 if succeeded, or -1 if failed
  */
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *current, *delete;
-	unsigned int i = 0;
 
-	current = *head;
+	unsigned int i = 0;
+	listint_t *tmp, *tmp2;
+
+	/* account for empty list */
 	if (*head == NULL)
-	{
 		return (-1);
-	}
+
+	tmp = *head;
+
+	/* account for deleting beginning node */
 	if (index == 0)
 	{
-#include "lists.h"
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
 
-/**
- * delete_nodeint_at_index - deletes node at index
- * @head: head
- * @index: index
- * Return: 1 if succeeded, -1 otherwise
- */
-
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
-{
-	listint_t *current, *delete;
-	unsigned int i = 0;
-
-	current = *head;
-	if (*head == NULL)
+	/* iterate tmp to idx prior to idx we want to delete */
+	while (i < (index - 1) && tmp != NULL)
 	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	/* account for idx out of range: don't delete and return */
+	if (i != (index - 1) || tmp->next == NULL)
 		return (-1);
-	}
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		free(current);
-	}
-	else
-	{
-		while (i < index - 1)
-		{
-			current = current->next;
-			if (current == NULL)
-				return (-1);
-			i++;
-		}
-		delete = current;
-		delete = delete->next;
-		current->next = delete->next;
-		free(delete);
-	}
-	return (1);
-}		*head = (*head)->next;
-		free(current);
-	}
-	else
-	{
-		while (i < index - 1)
-		{
-			current = current->next;
-			if (current == NULL)
-				return (-1);
-			i++;
-		}
-		delete = current;
-		delete = delete->next;
-		current->next = delete->next;
-		free(delete);
-	}
+
+	/* link prior idx before delete */
+	tmp2 = tmp->next;
+	tmp->next = (tmp->next)->next;
+	free(tmp2);
+
 	return (1);
 }
+
